5f642f0385c02bd814098370f0aaaf15490d3a11
abadfish <kirsten.ofarrell@gmail.com>
2016-05-21 17:54:47 -0400

    controllers and views passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  enable :sessions
+
+  get '/' do
+    erb :'application/root'
+  end
+
 end
